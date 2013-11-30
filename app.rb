dd9dcce8cb2976c6df63712d11eaf3f8ecc1b796
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-29 20:41:28 -0500

    Add tests in app_spec.rb and code in app.rb to set up objects and relationships for artist, songs and genres

--- app.rb
+++ app.rb
@@ -1,40 +1,87 @@
 require './lib/artist'
 require './lib/genre'
 require './lib/song'
+require 'debugger'
 
 def debrackafy(word)
 	word.gsub!(/\[/,"").gsub!(/\]/, "")
 end
 
-all_the_songs = Dir.entries("data").select {|f| !File.directory? f}
-# artist_name_regex = /(.*(?=\-))/
+def grab_data_from_dir(path)
+	Dir.entries(path).select {|f| !File.directory? f}
+end
+
+def create_regex_format_array(regex_format, values_array)
+	values_array.collect do |value|
+		regex_format.match(value).to_s.strip
+	end
+end
+
+def create_objects_array(object_type, number_of_objects)
+	object_array = []
+	number_of_objects.times do
+		object_array << object_type.new
+	end
+	object_array
+end
+
+def set_name_for_object_array(object_array, name_array)
+	object_array.each_with_index do |object, index|
+		object.name = name_array[index]
+	end
+end
+
+def add_genres_to_songs(song_objects_array, genre_objects_array)
+	song_objects_array.each_with_index do |song, index|
+		song.genre = genre_objects_array[index]
+	end
+end
+
+def add_song_to_artist(artist_object_array, song_objects_array)
+	artist_object_array.each_with_index do |artist, index| 
+		artist.add_song(song_objects_array[index])
+	end
+
+end
+
 artist_name_regex = /((.*\w*) (?=\-))/
 song_name_regex = /(?<=\-).*(?=\[)/
 genre_name_regex = /\[.*\]/
 
-artist_name = artist_name_regex.match(all_the_songs[0]).to_s.strip
-song_name = song_name_regex.match(all_the_songs[0]).to_s.strip
-genre_name = genre_name_regex.match(all_the_songs[0]).to_s.strip
+all_the_songs = grab_data_from_dir("data")
 
-artist1 = Artist.new
-song1 = Song.new
-genre1 = Genre.new
+artist_name_array = create_regex_format_array(artist_name_regex, all_the_songs)
+song_name_array = create_regex_format_array(song_name_regex, all_the_songs)
+genre_name_array = create_regex_format_array(genre_name_regex, all_the_songs)
 
-artist1.name = artist_name
-song1.name = song_name
-genre1.name = debrackafy(genre_name)
-
-artist_name_array = []
-song_name_array = []
-genre_name_array = []
-
-all_the_songs.each do |song|
-	artist_name_array << artist_name_regex.match(song).to_s.strip
-	song_name_array << song_name_regex.match(song).to_s.strip
-	genre_name_array << genre_name_regex.match(song).to_s.strip
+genre_name_array.collect do |genre|
+	debrackafy(genre)
 end
+#remove the brackets from genre captured by the genre_array_regex
 
-ap artist_name_array
-ap song_name_array
-ap genre_name_array
-ap all_the_songs
+#Step 1: Create new objects for each song, artist and genre
+artist_objects_array = create_objects_array(Artist, artist_name_array.length)
+
+song_objects_array = create_objects_array(Song, song_name_array.length)
+
+genre_objects_array = create_objects_array(Genre, genre_name_array.length)
+
+#Step 2: Add name to artist, song, genre
+artist_objects_array = set_name_for_object_array(artist_objects_array, artist_name_array)
+song_objects_array = set_name_for_object_array(song_objects_array, song_name_array)
+genre_objects_array = set_name_for_object_array(genre_objects_array, genre_name_array)
+
+#Step 3: Add genres to songs
+song_objects_array = add_genres_to_songs(song_objects_array, genre_objects_array)
+
+#Step 4: Add song to artist
+
+artist_objects_array = add_song_to_artist(artist_objects_array, song_objects_array)
+
+
+
+
+# ap artist_name_array
+# ap song_name_array
+# ap genre_name_array
+# ap all_the_songs
--- app_spec.rb
+++ app_spec.rb
@@ -5,3 +5,80 @@ describe "#debrackafy" do
 		expect(debrackafy("[Genre]")).to eq("Genre")
 	end
 end
+
+describe "#grab_data_from_dir" do
+	test_path = "data"
+	test_array_return = Dir.entries("data").select {|f| !File.directory? f}
+	it "should grab data from given path and return an array" do
+		expect(grab_data_from_dir(test_path)).to eq(test_array_return)
+	end
+end
+
+describe "#create_regex_format_array" do
+	it "should create array of just 'Artist Name' given format 'Artist Name - Song Name [genre_name].mp3' and appropriate regex format" do
+		artist_name_regex = /((.*\w*) (?=\-))/
+		test_array = ["A$AP Rocky - Peso [dance].mp3"]
+		expect(create_regex_format_array(artist_name_regex, test_array)).to eq(["A$AP Rocky"])
+	end
+end
+
+describe "#create_objects_array" do
+	it "should create an object array equal to new of items in given array" do
+		test_object_type = Artist
+		test_name_array = ["A$AP Rocky", "Kdizzle"]
+		expect(create_objects_array(test_object_type, test_name_array.length)[0]).to be_a(Artist)
+	end
+end
+
+describe "#set_name_for_object_array" do
+	it "should set the name of the objects with given values of name array" do
+		test_object_array = [Artist.new]
+		test_name_array = ["A$AP Rocky", "Kdizzle"]
+		expect(set_name_for_object_array(test_object_array, test_name_array)[0].name).to eq("A$AP Rocky")
+	end
+end
+
+describe "#add_genres_to_songs" do
+	it "should add genre to song" do
+		test_song = Song.new
+		test_song.name = "Kdizzle"
+		test_song_2 = Song.new
+		test_song_2.name = "Kdizzle_2"
+		test_genre = Genre.new
+		test_genre.name = "Hard Rock"
+		test_genre_2 = Genre.new
+		test_genre_2.name = "Hip-Hop"
+		test_song_object_array = [test_song, test_song_2]
+		test_genre_objects_array = [test_genre, test_genre_2]
+
+		expect(add_genres_to_songs(test_song_object_array,test_genre_objects_array)[0].genre.name).to eq("Hard Rock")
+	end
+end
+
+describe "#add_song_to_artist" do
+	it "should add song to artist" do
+		test_song = Song.new
+		test_song.name = "Kdizzle"
+		test_song_2 = Song.new
+		test_song_2.name = "Kdizzle_2"
+		test_genre = Genre.new
+		test_genre.name = "Hard Rock"
+		test_genre_2 = Genre.new
+		test_genre_2.name = "Hip-Hop"
+		test_song_object_array = [test_song, test_song_2]
+		test_genre_objects_array = [test_genre, test_genre_2]
+		test_artist = Artist.new
+		test_artist.name = "Kphilly"
+		test_artist_2 = Artist.new
+		test_artist_2.name = "Kphilly_2"
+		test_artist_array = [test_artist, test_artist_2]
+
+		test_song_object_array =  
+		add_genres_to_songs(test_song_object_array,test_genre_objects_array)
+
+		expect(add_song_to_artist(test_artist_array, test_song_object_array)[1].songs[0].name).to eq("Kdizzle_2")
+
+		expect(add_song_to_artist(test_artist_array, test_song_object_array)[0].genres[0].name).to eq("Hard Rock")
+	end
+end
+
